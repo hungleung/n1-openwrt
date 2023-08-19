fc3ebb211bdaf6261789e4d024b403237493ad4e
hungleung <hungleung.lee@me.com>
2023-08-19 20:33:37 +0800

    22.03

---
+++ config/openwrt-openwrt-22.03/diy-part2.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+#========================================================================================================================
+# https://github.com/ophub/amlogic-s9xxx-openwrt
+# Description: Automatically Build OpenWrt
+# Function: Diy script (After Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
+# Source code repository: https://github.com/openwrt/openwrt / Branch: main
+#========================================================================================================================
+
+# ------------------------------- Main source started -------------------------------
+#
+# Add the default password for the 'root' user（Change the empty password to 'password'）
+sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
+
+# Set etc/openwrt_release
+sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
+echo "DISTRIB_SOURCECODE='official'" >>package/base-files/files/etc/openwrt_release
+
+# Modify default IP（FROM 192.168.1.1 CHANGE TO 192.168.31.4）
+# sed -i 's/192.168.1.1/192.168.31.4/g' package/base-files/files/bin/config_generate
+#
+# ------------------------------- Main source ends -------------------------------
+
+# ------------------------------- Other started -------------------------------
+#
+# Add luci-app-amlogic
+svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
+
+# remove v2ray-geodata package from feeds (openwrt-22.03 & master)
+rm -rf feeds/packages/net/v2ray-geodata
+
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+
+# coolsnowwolf default software package replaced with Lienol related software package
+# rm -rf feeds/packages/utils/{containerd,libnetwork,runc,tini}
+# svn co https://github.com/Lienol/openwrt-packages/trunk/utils/{containerd,libnetwork,runc,tini} feeds/packages/utils
+
+# Add third-party software packages (The entire repository)
+# git clone https://github.com/libremesh/lime-packages.git package/lime-packages
+# Add third-party software packages (Specify the package)
+# svn co https://github.com/libremesh/lime-packages/trunk/packages/{shared-state-pirania,pirania-app,pirania} package/lime-packages/packages
+# Add to compile options (Add related dependencies according to the requirements of the third-party software package Makefile)
+# sed -i "/DEFAULT_PACKAGES/ s/$/ pirania-app pirania ip6tables-mod-nat ipset shared-state-pirania uhttpd-mod-lua/" target/linux/armvirt/Makefile
+
+# Apply patch
+# git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
+#
+# ------------------------------- Other ends -------------------------------
+
+
+# sed -i 's/192.168.1.1/192.168.88.2/g' package/base-files/files/bin/config_generate
+# sed -i 's/OpenWrt/Home/g' package/base-files/files/bin/config_generate
+# sed -i 's/\+shellsync//' package/network/services/ppp/Makefile
+# sed -i 's/\+kmod-mppe//' package/network/services/ppp/Makefile
+# sed -i '281s/y/n/'  config/Config-images.in
+# sed -i '293s/y/n/'  config/Config-images.in
+# sed -i '70s/y/n/'  config/Config-images.in
+# sed -i '80s/y/n/'  config/Config-images.in
+# sed -i 's/Dynamic DNS/DDNS/g'  feeds/luci/applications/luci-app-ddns/luasrc/controller/ddns.lua
+# sed -i 's/ACME certs/ACME/' feeds/luci/applications/luci-app-acme/luasrc/controller/acme.lua
+# sed -i 's/_("udpxy")/_("IPTV")/' feeds/luci/applications/luci-app-udpxy/luasrc/controller/udpxy.lua 
+# sed -i '12-15d' feeds/luci/applications/luci-app-acme/po/zh-cn/acme.po
+
+sed -i -e '56s/dnsmasq/dnsmasq-full vpn-policy-routing luci-app-vpn-policy-routing luci-app-mosdns luci-app-wireguard luci-proto-wireguard wireguard-tools kmod-wireguard perl perl-http-date perlbase-file perlbase-getopt perlbase-time perlbase-unicode perlbase-utf8 blkid fdisk lsblk parted attr btrfs-progs chattr dosfstools e2fsprogs f2fs-tools f2fsck lsattr mkf2fs xfs-fsck xfs-mkfs bsdtar bash gawk getopt losetup tar uuidgen luci-app-amlogic/' include/target.mk
