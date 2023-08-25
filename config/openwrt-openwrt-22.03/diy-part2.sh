1baf39a0387c989d21f8306884ec871ba74982be
hungleung <hungleung.lee@me.com>
2023-08-25 19:20:54 +0800

    openwrt-22.03

--- config/openwrt-openwrt-22.03/diy-part2.sh
+++ config/openwrt-openwrt-22.03/diy-part2.sh
@@ -55,13 +55,10 @@ sed -i 's/192.168.1.1/192.168.88.2/g' package/base-files/files/bin/config_genera
 sed -i 's/OpenWrt/Home/g' package/base-files/files/bin/config_generate
 sed -i 's/\+shellsync//' package/network/services/ppp/Makefile
 sed -i 's/\+kmod-mppe//' package/network/services/ppp/Makefile
-# sed -i '281s/y/n/'  config/Config-images.in
-# sed -i '293s/y/n/'  config/Config-images.in
-# sed -i '70s/y/n/'  config/Config-images.in
-# sed -i '80s/y/n/'  config/Config-images.in
 sed -i 's/Dynamic DNS/DDNS/g'  feeds/luci/applications/luci-app-ddns/luasrc/controller/ddns.lua
 sed -i 's/ACME certs/ACME/' feeds/luci/applications/luci-app-acme/luasrc/controller/acme.lua
 sed -i 's/_("udpxy")/_("IPTV")/' feeds/luci/applications/luci-app-udpxy/luasrc/controller/udpxy.lua 
-sed -i '12-15d' feeds/luci/applications/luci-app-acme/po/zh-cn/acme.po
+sed -i '12,15d' feeds/luci/applications/luci-app-acme/po/zh-cn/acme.po
 
 sed -i -e '56s/dnsmasq/luci-compat luci-app-mosdns adguardhome dnsmasq-full luci-app-wireguard luci-proto-wireguard wireguard-tools kmod-wireguard perl perl-http-date perlbase-file perlbase-getopt perlbase-time perlbase-unicode perlbase-utf8 blkid fdisk lsblk parted attr btrfs-progs chattr dosfstools e2fsprogs f2fs-tools f2fsck lsattr mkf2fs xfs-fsck xfs-mkfs bsdtar bash gawk getopt losetup tar uuidgen luci-app-amlogic/' include/target.mk
+sed -i '62,63d' include/target.mk
