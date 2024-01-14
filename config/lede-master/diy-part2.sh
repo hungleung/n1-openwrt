75605265d1dfbbe97bb14eccebd9cf4db41cef7c
hungleung <hungleung.lee@me.com>
2024-01-14 17:42:59 +0800

    remove nfs

--- config/lede-master/diy-part2.sh
+++ config/lede-master/diy-part2.sh
@@ -94,6 +94,6 @@ sed -i 's/nas/services/g' feeds/luci/applications/luci-app-samba4/luasrc/control
 sed -i -e '56s/firewall iptables ppp ppp-mod-pppoe/adguardhome luci-app-qbittorrent/' include/target.mk
 sed -i -e '57s/block-mount coremark kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw kmod-tun/luci-app-samba4/' include/target.mk
 sed -i -e '58s/iptables-mod-tproxy iptables-mod-extra ipset ip-full default-settings/nano htop curl/' include/target.mk
-sed -i -e '59s/ddns-scripts_aliyun ddns-scripts_dnspod luci-app-ddns luci-app-upnp luci-app-autoreboot/nfs-utils kmod-fs-nfs kmod-fs-nfs-v4 kmod-fs-nfs-v3 nfs-kernel-server kmod-loop/' include/target.mk
+sed -i -e '59s/ddns-scripts_aliyun ddns-scripts_dnspod luci-app-ddns luci-app-upnp luci-app-autoreboot//' include/target.mk
 sed -i -e '60s/luci-app-arpbind luci-app-filetransfer luci-app-vsftpd luci-app-ssr-plus/luci-app-amlogic perl perl-http-date perlbase-file perlbase-getopt perlbase-time perlbase-unicode perlbase-utf8 blkid fdisk lsblk parted attr btrfs-progs chattr dosfstools e2fsprogs f2fs-tools f2fsck lsattr mkf2fs xfs-fsck xfs-mkfs bsdtar bash gawk getopt losetup tar uuidgen/' include/target.mk
-sed -i -e '61s/luci-app-accesscontrol luci-app-nlbwmon luci-app-turboacc luci-app-wol curl ca-certificate/luci-app-nfs/' include/target.mk
+sed -i -e '61s/luci-app-accesscontrol luci-app-nlbwmon luci-app-turboacc luci-app-wol curl ca-certificate//' include/target.mk
